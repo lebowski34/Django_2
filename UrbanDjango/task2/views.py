978fa3e57d9c57b95771f6b64ce7e6486ea2a993
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-12 04:52:16 +0300

    Add files via upload

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views import View
+
+# Функциональное представление
+def func_view(request):
+    return render(request, 'second_task/func_template.html')
+
+# Классовое представление
+class ClassView(View):
+    def get(self, request):
+        return render(request, 'second_task/class_template.html')
